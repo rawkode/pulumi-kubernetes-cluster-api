a78da7934940da3133e0be3f19b5355ed39c7335
David McKay <david@rawkode.dev>
2021-01-15 12:21:31 +0000

    docs: create secret resource set

--- cluster-api-example/index.ts
+++ cluster-api-example/index.ts
@@ -1,5 +1,6 @@
 import * as kubernetes from "@pulumi/kubernetes";
 import { init } from "@rawkode/pulumi-kubernetes-cluster-api";
+import { createSecretResourceSet } from "@rawkode/pulumi-kubernetes-cluster-api/bin/resource-sets";
 
 const kubernetesProvider = new kubernetes.Provider("kubernetes", {
 	renderYamlToDirectory: "rendered-yaml",
@@ -9,3 +10,15 @@ export const clusterApi = init({
 	installCertManager: false,
 	kubernetesProvider,
 });
+
+export const secretResourceSet = createSecretResourceSet(
+	kubernetesProvider,
+	"my-secret",
+	"default",
+	[
+		{
+			key: "my-key",
+			value: "my-value",
+		},
+	]
+);
