3d8ae292153e3e39d12ef792530c6437d088b56b
David McKay <david@rawkode.dev>
2021-01-18 16:54:41 +0000

    chore: use workspace dependencies

--- cluster-api/src/runtimes/index.ts
+++ cluster-api/src/runtimes/index.ts
@@ -1,4 +1,4 @@
 export enum Runtime {
-  Docker,
-  containerd,
+	Docker,
+	containerd,
 }
--- providers/packet-example/index.ts
+++ providers/packet-example/index.ts
@@ -1,18 +1,45 @@
 import * as kubernetes from "@pulumi/kubernetes";
 import * as capi from "@rawkode/pulumi-kubernetes-cluster-api";
-import { init } from "@rawkode/pulumi-kubernetes-cluster-api-packet";
+import {
+	init,
+	createControlPlane,
+} from "@rawkode/pulumi-kubernetes-cluster-api-packet";
+import Facility from "@rawkode/pulumi-kubernetes-cluster-api-packet/facility";
+import IPCidr from "ip-cidr";
 
 const kubernetesProvider = new kubernetes.Provider("kubernetes", {
 	renderYamlToDirectory: "rendered-yaml",
 });
 
-const { clusterApi } = capi.init({
-	installCertManager: false,
+// const { clusterApi } = capi.init({
+// 	installCertManager: false,
+// 	kubernetesProvider,
+// });
+
+// const packet = init({
+// 	apiKey: "abc123",
+// 	clusterApi,
+// 	kubernetesProvider,
+// });
+
+const clusterName = "rawkode";
+const projectID = "";
+
+const { cluster, controlPlane } = createControlPlane({
+	facility: Facility.AM6,
 	kubernetesProvider,
+	name: clusterName,
+	projectID,
 });
 
-const packet = init({
-	apiKey: "abc123",
-	clusterApi,
-	kubernetesProvider,
-});
+const myCluster = capi.createCluster(
+	{
+		name: "rawkodes-cluster",
+		kubernetesVersion: "1.19.4",
+		podCidr: new IPCidr("10.0.0.0/24"),
+		serviceCidr: new IPCidr("192.168.86.0/24"),
+		kubernetesProvider,
+	},
+	cluster,
+	controlPlane
+);
