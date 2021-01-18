e479b1652edb3c6b98f00eaca2eecc3e1ee180b8
David McKay <david@rawkode.dev>
2021-01-18 11:56:29 +0000

    fix: use kubernetes metadata type instead of creating our own

--- cluster-api/src/cluster/infrastructure-provider.ts
+++ cluster-api/src/cluster/infrastructure-provider.ts
@@ -1,11 +1,10 @@
 import { CustomResource, Output } from "@pulumi/pulumi";
+import { types } from "@pulumi/kubernetes";
 
 export type InfrastructureProvider = CustomResource & InfrastructureCluster;
 
 interface InfrastructureCluster {
 	apiVersion: Output<string | undefined>;
 	kind: Output<string | undefined>;
-	metadata: {
-		name: Output<string | undefined>;
-	};
+	metadata: types.input.meta.v1.ObjectMeta;
 }
